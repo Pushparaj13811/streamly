4e7cc4fb876c621d898986825a28d83d23c7f7bf
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-07-24 09:16:43 +0530

    created some utility files {apiError, apiResponse, asyncHandler and cloudinary}

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         return await fn(req, res, next);
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message || "Server Error",
+//         });
+//     }
+// };
+
+// for promises
+
+const asyncHandler = (requestHandler) => {
+   return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) =>
+            next(err)
+        );
+    };
+};
+
+export { asyncHandler };
