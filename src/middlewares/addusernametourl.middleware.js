e08a8b72cc108f4df6d4473624c7ef5ec75addd2
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-07-28 21:37:26 +0530

    refactor: Remove unnecessary console.log statements and add username to URL params
    This commit removes unnecessary console.log statements from the user.controller.js file. Additionally, it adds the username to the URL params in the addUsernameToParams middleware. These changes improve the code readability and ensure that the username is correctly passed in the URL params when required.

--- src/middlewares/addusernametourl.middleware.js
+++ src/middlewares/addusernametourl.middleware.js
@@ -1,4 +1,5 @@
 export const addUsernameToParams = (req, res, next) => {
+    const username = req.params.username;
     if (req.user && req.user.username) {
         req.params.username = req.user.username;
     }
