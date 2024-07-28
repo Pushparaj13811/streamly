e2d54b1973afe6f53f1d1e47441e7c18debd16e0
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-07-28 10:39:10 +0530

    ```text refactor: Update video.controller.js to import ApiResponse from the correct path
    The video.controller.js file has been updated to import ApiResponse from the correct path in the utils folder. This change ensures that the ApiResponse class is imported correctly and used in the video controller for handling API responses.

--- src/controllers/video.contoller.js
+++ src/controllers/video.contoller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
-import { ApiResponse } from "../utils/apiResponse.js";
+import ApiResponse from "../utils/apiResponse.js";
 import {
     uploadOnCloudinary,
     deleteFromCloudinary,
