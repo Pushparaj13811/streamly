c6f295ebd9aea1a4aa96c279bf6d60afcd2c70e8
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-07-28 14:13:52 +0530

    feat: Add createComment, getVideoComments, updateComment, and deleteComment functions
    This commit adds the createComment, getVideoComments, updateComment, and deleteComment functions to the comment.controller.js file. These functions handle the creation, retrieval, updating, and deletion of comments respectively. They include necessary validation and error handling logic. This addition enhances the functionality of the application by providing the necessary endpoints for comment management.

--- src/controllers/video.contoller.js
+++ src/controllers/video.contoller.js
@@ -6,33 +6,51 @@ import {
     deleteFromCloudinary,
 } from "../utils/cloudinary.js";
 import { Video } from "../models/video.model.js";
+import getVideoDuration from "../utils/duration.js";
+import mongoose from "mongoose";
 
 const createVideo = asyncHandler(async (req, res) => {
     const { title, description, isPublished } = req.body;
 
+    const videoInfo = await Video.findOne({ title });
+
     if (!title) {
         throw new ApiError(400, "Title is required");
+    }
+    if (videoInfo) {
+        throw new ApiError(400, "Title already exists");
     }
     if (!description) {
         throw new ApiError(400, "Description is required");
     }
 
-    if (isPublished != false) {
-        isPublished = true;
-    }
+    const published = isPublished !== false;
 
     const videoLocalPath = req.files?.video[0].path;
     const thumbnailLocalPath = req.files?.thumbnail[0].path;
 
+    const duration = await getVideoDuration(videoLocalPath)
+        .then((duration) => {
+            return duration;
+        })
+        .catch((error) => {
+            throw new ApiError(500, "Failed to get video duration");
+        });
+
+    console.log("duration", duration);
+
+    if (!duration) {
+        throw new ApiError(500, "Failed to get video duration");
+    }
+
     if (!videoLocalPath) {
         throw new ApiError(400, "Video is required");
     }
+    const video = await uploadOnCloudinary(videoLocalPath);
 
     if (!thumbnailLocalPath) {
         throw new ApiError(400, "Thumbnail is required");
     }
-
-    const video = await uploadOnCloudinary(videoLocalPath);
     const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
 
     if (!video) {
@@ -45,11 +63,13 @@ const createVideo = asyncHandler(async (req, res) => {
     const owner = req.user?._id;
 
     const uploadVideo = await Video.create({
-        videoFile: video,
-        thumbnail: thumbnail,
+        videoFile: video?.url,
+        thumbnail: thumbnail?.url,
         title,
         description,
-        isPublished,
+        isPublished: published,
+        views: 0,
+        duration: duration,
         owner,
     });
 
@@ -59,7 +79,6 @@ const createVideo = asyncHandler(async (req, res) => {
             new ApiResponse(200, { uploadVideo }, "Video uploaded successfully")
         );
 });
-
 const getAllVideo = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
 
@@ -73,37 +92,110 @@ const getAllVideo = asyncHandler(async (req, res) => {
         options.sort = {
             [sortBy]: sortType,
         };
+    } else {
+        options.sort = {
+            createdAt: -1,
+        };
     }
 
-    const queryOptions = {};
+    const match = {
+        isPublished: true,
+    };
 
     if (userId) {
-        queryOptions.owner = userId;
+        match.owner = userId;
     }
 
     if (query) {
-        queryOptions.title = { $regex: query, $options: "i" };
+        match.title = { $regex: query, $options: "i" };
     }
 
+    const aggregate = [
+        { $match: match },
+        { $sort: options.sort },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "ownerDetails",
+            },
+        },
+        { $unwind: "$ownerDetails" },
+        {
+            $project: {
+                owner: "$ownerDetails.fullName",
+                ownerId: "$ownerDetails._id",
+                ownerUsername: "$ownerDetails.username",
+                videoFile: 1,
+                thumbnail: 1,
+                title: 1,
+                description: 1,
+                duration: 1,
+                views: 1,
+                isPublished: 1,
+                createdAt: 1,
+                updatedAt: 1,
+            },
+        },
+    ];
+
     try {
-        const video = await Video.aggregatePaginate(queryOptions, options);
+        const videos = await Video.aggregatePaginate(aggregate, options);
 
         return res
             .status(200)
             .json(
-                new ApiResponse(200, { video }, "Videos fetched successfully")
+                new ApiResponse(200, { videos }, "Videos fetched successfully")
             );
     } catch (error) {
-        throw new ApiError(500, "Failed to fetch video");
+        console.log(error);
+        throw new ApiError(500, "Failed to fetch videos : ", error);
     }
 });
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { id } = req.params;
 
-    const video = await Video.findById(id);
+    const match = {
+        _id: new mongoose.Types.ObjectId(id),
+        isPublished: true,
+    };
 
-    if (!video) {
+    const aggregate = [
+        { $match: match },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "ownerDetails",
+            },
+        },
+        { $unwind: "$ownerDetails" },
+        {
+            $project: {
+                owner: "$ownerDetails.fullName",
+                ownerId: "$ownerDetails._id",
+                ownerUsername: "$ownerDetails.username",
+                videoFile: 1,
+                thumbnail: 1,
+                title: 1,
+                description: 1,
+                duration: 1,
+                views: 1,
+                isPublished: 1,
+                createdAt: 1,
+                updatedAt: 1,
+            },
+        },
+    ];
+
+    const video = await Video.aggregate(aggregate);
+
+    console.log("video", video);
+
+    if (!video.length) {
         throw new ApiError(404, "Video not found");
     }
 
@@ -113,8 +205,16 @@ const getVideoById = asyncHandler(async (req, res) => {
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
-    const id = req.video?._id;
+    const id = req.params.id;
+    console.log("id", id);
     const { title, description, isPublished } = req.body;
+
+    console.log("req.video", req.video);
+    console.log("req.body", req.body);
+
+    if (!id) {
+        throw new ApiError(400, "Video ID is required");
+    }
 
     if (!title) {
         throw new ApiError(400, "Title is required");
@@ -123,11 +223,8 @@ const updateVideo = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Description is required");
     }
 
-    if (isPublished !== false) {
-        isPublished = true;
-    }
-
-    const video = await Video.findById(id);
+    let video = await Video.findById(id);
+    console.log("video", video);
 
     if (!video) {
         throw new ApiError(404, "Video not found");
@@ -135,7 +232,7 @@ const updateVideo = asyncHandler(async (req, res) => {
 
     video.title = title;
     video.description = description;
-    video.isPublished = isPublished;
+    video.isPublished = isPublished || video.isPublished;
 
     await video.save();
 
@@ -145,12 +242,17 @@ const updateVideo = asyncHandler(async (req, res) => {
 });
 
 const deleteVideo = asyncHandler(async (req, res) => {
-    const id = req.params.id;
+    const id = req.params?.id;
+    const userId = req.user?._id;
 
     const video = await Video.findById(id);
 
     if (!video) {
         throw new ApiError(404, "Video not found");
+    }
+
+    if (userId !== video.owner.toString()) {
+        throw new ApiError(403, "You are not authorized to delete this video");
     }
 
     const videoPublicId = video.videoFile.split("/").pop().split(".")[0];
@@ -161,7 +263,7 @@ const deleteVideo = asyncHandler(async (req, res) => {
         throw new ApiError(500, "Failed to delete video");
     }
 
-    await video.remove();
+    await video.deleteOne({ _id: video._id });
 
     return res
         .status(200)
@@ -169,9 +271,9 @@ const deleteVideo = asyncHandler(async (req, res) => {
 });
 
 const togglePublishedVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params;
+    const { id } = req.params;
 
-    const video = await Video.findById(videoId);
+    const video = await Video.findById(id);
 
     if (!video) {
         throw new ApiError(404, "Video not found");
