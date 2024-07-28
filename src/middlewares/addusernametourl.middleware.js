fb4669a96af3c102a49c18a28bb6f3338217dc38
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-07-28 21:37:54 +0530

    refactor: Remove unnecessary console.log statements and add username to URL params

--- src/middlewares/addusernametourl.middleware.js
+++ src/middlewares/addusernametourl.middleware.js
@@ -1,5 +1,4 @@
 export const addUsernameToParams = (req, res, next) => {
-    const username = req.params.username;
     if (req.user && req.user.username) {
         req.params.username = req.user.username;
     }
