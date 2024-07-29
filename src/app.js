932f1a12c8506a1712d78b0042125936b7415e4d
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-07-29 15:58:49 +0530

    feat: Add playlist routes for managing playlists

--- src/app.js
+++ src/app.js
@@ -36,6 +36,7 @@ import commentRouter from "./routes/comment.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import subscriptionRouter from "./routes/subscriptions.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
 
 // routes declaration
 
@@ -45,5 +46,6 @@ app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/playlists", playlistRouter);
 
 export { app };
